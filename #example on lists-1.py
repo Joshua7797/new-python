4abd76a9b8c4348bf15e68ab8dfd0dce8766948d
joshua <joshuapushparaj97@gmail.com>
2024-03-16 17:59:18 +0530

    existing files uploaded to hub

---
+++ #example on lists-1.py
@@ -0,0 +1,7 @@
+#example on lists
+
+raj= [10,-20,15.5,63,75,87,99,87,63,101,'Vijay',"Roopa"]
+print(raj)
+print(raj[5])
+print(raj[4:8])
+print(raj[-5])
---
+++ multiply.py
@@ -0,0 +1,4 @@
+num = int(input("Display multiplication table of: "))
+
+for i in range(1, 21):
+    print(f"{num} X {i} = {num*i}")
